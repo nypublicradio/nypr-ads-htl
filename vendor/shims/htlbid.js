8ae8aac15384c3472d7a598ada4d8ea735d49e3d
walsh9 <walsh9@gmail.com>
2019-11-15 12:26:34 -0500

    add stub for removeslot

--- vendor/shims/htlbid.js
+++ vendor/shims/htlbid.js
@@ -5,6 +5,7 @@
     var htlbid = self['htlbid'] = self['htlbid'] || {};
     htlbid.cmd = htlbid.cmd || [];
     htlbid.on = htlbid.on || function() {};
+    htlbid.removeSlot = htlbid.removeSlot || function() {};
 
     return {
       'default': self['htlbid'],
